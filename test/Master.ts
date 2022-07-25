9514fba6324cc1c7806a6735917a4ce411e3649c
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-07-25 17:00:18 +0530

    test: added tests for the new registration checks and update player

--- test/Master.ts
+++ test/Master.ts
@@ -15,10 +15,70 @@ describe("Player", async () => {
   it("should create a player", async () => {
     const { contract, owner } = await loadFixture(deployContract);
 
-    const username = "lol a random username";
+    const username = "random username";
 
     expect(await contract.registerPlayer(username))
       .to.emit(contract, "NewPlayer")
       .withArgs(owner.address, username);
+  });
+
+  it("should not create a player with an existing username", async () => {
+    const { contract } = await loadFixture(deployContract);
+
+    const username = "random username";
+
+    await contract.registerPlayer(username);
+
+    await expect(contract.registerPlayer(username)).to.be.revertedWith(
+      "Username already taken"
+    );
+  });
+
+  it("should not create a player with a username less than 3 characters", async () => {
+    const { contract } = await loadFixture(deployContract);
+
+    const username = "gm";
+
+    await expect(contract.registerPlayer(username)).to.be.revertedWith(
+      "Username must be between 3 and 20 characters"
+    );
+  });
+
+  it("should not create a player with a username more than 20 characters", async () => {
+    const { contract } = await loadFixture(deployContract);
+
+    const username = "abcdefghijklmnopqrstuvwxyz";
+
+    await expect(contract.registerPlayer(username)).to.be.revertedWith(
+      "Username must be between 3 and 20 characters"
+    );
+  });
+
+  it("should update a player's username", async () => {
+    const { contract, owner } = await loadFixture(deployContract);
+
+    const username = "random username";
+
+    await contract.registerPlayer(username);
+
+    const newUsername = "new username";
+
+    expect(await contract.updatePlayer(newUsername))
+      .to.emit(contract, "PlayerProfileUpdated")
+      .withArgs(owner.address, username, newUsername);
+  });
+
+  it("should not create a player with different username if address already has a player", async () => {
+    const { contract } = await loadFixture(deployContract);
+
+    const username = "random username";
+
+    await contract.registerPlayer(username);
+
+    const newUsername = "new username";
+
+    await expect(contract.registerPlayer(newUsername)).to.be.revertedWith(
+      "You already have a player"
+    );
   });
 });
