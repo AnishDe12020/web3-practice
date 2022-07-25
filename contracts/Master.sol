2b02703618f0e1896552e00c9755bb2bfa9eebb4
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-07-25 16:58:12 +0530

    feat: add checks for registering player
    made sure that player with same username doesn't exist
    made sure that username is between 3 and 20 chars
    made sure that user owns only 1 player
    update update player function name and derive owner address from msg.sender

--- contracts/Master.sol
+++ contracts/Master.sol
@@ -4,8 +4,15 @@ pragma solidity ^0.8.9;
 // Import this file to use console.log
 import "hardhat/console.sol";
 
+import {StringUtils} from "./libraries/StringUtils.sol";
+
 contract Master {
     event NewPlayer(address ownerAddress, string username);
+    event PlayerProfileUpdated(
+        address ownerAddress,
+        string oldUsername,
+        string newUsername
+    );
 
     struct ShopItem {
         string name;
@@ -20,6 +27,8 @@ contract Master {
 
     Player[] public players;
     ShopItem[] public shop;
+
+    mapping(address => uint256) public addressToPlayerIndex;
 
     constructor() {
         addMockShopData();
@@ -46,21 +55,58 @@ contract Master {
         shop.push(ShopItem("Cupcake", 160));
     }
 
+    function isUsernameTaken(string memory username)
+        public
+        view
+        returns (bool)
+    {
+        for (uint256 i = 0; i < players.length; i++) {
+            if (
+                keccak256(abi.encodePacked(players[i].username)) ==
+                keccak256(abi.encodePacked(username))
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function addressHasPlayer(address ownerAddress) public view returns (bool) {
+        for (uint256 i = 0; i < players.length; i++) {
+            if (players[i].ownerAddress == ownerAddress) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     function registerPlayer(string memory username) public {
+        // Check if username is between 3 and 20 characters
+        require(
+            StringUtils.strlen(username) >= 3 &&
+                StringUtils.strlen(username) <= 20,
+            "Username must be between 3 and 20 characters"
+        );
+
+        // Check if the username is already taken
+        require(isUsernameTaken(username) == false, "Username already taken");
+        // One player per address
+        require(
+            addressHasPlayer(msg.sender) == false,
+            "You already have a player"
+        );
+
         Player memory player = Player(msg.sender, username, 0);
 
         players.push(player);
+        addressToPlayerIndex[msg.sender] = players.length - 1;
         emit NewPlayer(msg.sender, username);
     }
 
-    function updateProfile(address _ownerAddress, string memory newUsername)
-        public
-    {
-        for (uint256 i = 0; i < players.length; i++) {
-            if (players[i].ownerAddress == _ownerAddress) {
-                players[i].username = newUsername;
-                break;
-            }
-        }
+    function updatePlayer(string memory newUsername) public {
+        Player memory player = players[addressToPlayerIndex[msg.sender]];
+        string memory oldUsername = player.username;
+        player.username = newUsername;
+        emit PlayerProfileUpdated(msg.sender, oldUsername, newUsername);
     }
 }
